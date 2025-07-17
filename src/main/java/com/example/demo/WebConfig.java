9477126ae9003585ad1f584ddde45871f567f91d
latte3cup <latte3cup@naver.com>
2025-07-17 10:32:40 +0900

    4th commit

---
+++ src/main/java/com/example/demo/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/greeting")
+                .allowedOrigins("http://192.168.20.163:3000")
+                .allowedMethods("GET");
+    }
+}
